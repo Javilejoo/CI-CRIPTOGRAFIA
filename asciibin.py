c92774dcb99f461076d31c3780b0720699c0f44d
Javier Prado <pra21486@uvg.edu.gt>
2026-01-22 18:14:21 -0600

    [ADD] correcion de binario

--- asciibin.py
+++ asciibin.py
@@ -10,7 +10,27 @@ print(lista)
 def ascii_to_bin(ascii_list):
     binary_list = []
     for num in ascii_list:
-        binary_list.append(bin(num)[2:].zfill(8))
+        bits = []  # lista para guardar los residuos
+        n = num
+        
+        # caso especial si el número es 0
+        if n == 0:
+            bits = ['0']
+        else:
+            while n > 0:
+                residuo = n % 2  # obtener el residuo de la división
+                bits.append(str(residuo))
+                n = n // 2  # división entera
+        
+        # invertir porque los residuos salen al revés
+        bits.reverse()
+        
+        # completar a 8 bits agregando 0s a la izquierda
+        while len(bits) < 8:
+            bits.insert(0, '0')
+        
+        binary_list.append(''.join(bits))
+            
     return binary_list
 
 def decimal_to_binary(n):
