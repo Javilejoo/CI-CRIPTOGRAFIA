9a63e5cb2bcb3a9d8612533983438a0b414119cb
Javier Prado <pra21486@uvg.edu.gt>
2026-01-27 19:52:13 -0600

    [UPDATE] add base 64

--- asciibin.py
+++ asciibin.py
@@ -4,7 +4,7 @@ def string_to_ascii(s):
     for char in s:
         lista.append(ord(char))
 
-string_to_ascii("hola")
+string_to_ascii("Sun")
 print(lista)
 
 def ascii_to_bin(ascii_list):
@@ -52,3 +52,45 @@ def decimal_to_binary(n):
 # convertir todos los elemento de la lista ASCII
 binary_list = ascii_to_bin(lista)
 print(binary_list)
+
+# Concatenar lista binary list
+def concatenar_lista(binary_list):
+    return ''.join(map(str, binary_list))
+
+concatenado = concatenar_lista(binary_list)
+print(concatenado)
+
+def bloques_de_6(cadena):
+    bloques = []
+    for i in range(0, len(cadena), 6):
+        bloques.append(cadena[i:i+6])
+    return bloques
+
+bloquesix = bloques_de_6(concatenado)
+print(bloques_de_6(concatenado))
+
+def binario_a_decimal(binario):
+    decimal = 0
+    longitud = len(binario)
+
+    for i in range(longitud):
+        bit = int(binario[longitud - 1 - i])
+        decimal += bit * (2 ** i)
+
+    return decimal
+
+
+decimales = [binario_a_decimal(b) for b in bloquesix]
+print(decimales)
+
+base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+
+# Función para obtener el carácter Base64 dado un índice decimal
+def decimal_to_base64_char(indice):
+    return base64_chars[indice]
+
+# Usando la lista de decimales (índices) para obtener los caracteres base64
+base64_string = ''.join([decimal_to_base64_char(d) for d in decimales])
+print("Base64:", base64_string)
+
+
